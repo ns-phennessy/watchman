1eaef4df99b459d8247ff00cf35bf7c2d8f9e15a
Patrick Hennessy <ph7vc@mail.missouri.edu>
2015-01-21 21:03:24 -0600

    Added pretty colors, keyboard interupt handling

--- watchman.py
+++ watchman.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python
-import os
-import time
+import os, sys, time
 from zlib import adler32
+from blessings import Terminal
 
-class FileWatcher:
+class Watchman:
 
-    def watch(self, dir=".", callbacks=None):        
+    def watch(self, dir=".", ignoreHidden=True, callbacks=None):        
         oldHashes = {}
         newHashes = {}
         
         if not callbacks:
-            callbacks = {"onChange": self.__onChange, "onRemove": self.__onRemove, "onAdd": self.__onAdd}
+            callbacks = {
+                "onChange": self.__onChange, 
+                "onRemove": self.__onRemove, 
+                "onAdd": self.__onAdd
+            }
         
-        print "Listening for file system changes..."
+        self.__print("Watching for file system changes.")
         
         while True:
-            filenames = self.__getFiles(dir)
-        
+            filenames = self.__getFiles(dir, ignoreHidden)
+            
+            # Check for file deletions
             if(len(oldHashes.keys()) > len(filenames)):
                 filesRemoved = list(set(oldHashes.keys()) - set(filenames))
 
@@ -25,7 +30,9 @@ class FileWatcher:
                     del(newHashes[file])
                     
                 callbacks["onRemove"](filesRemoved)
-                 
+                continue
+            
+            # Check for file additions
             elif(len(oldHashes.keys()) < len(filenames)):                
                 filesAdded = list(set(filenames) - set(oldHashes.keys()))
 
@@ -34,32 +41,39 @@ class FileWatcher:
                     newHashes[file] = oldHashes[file]
                     
                 callbacks["onAdd"](filesAdded)
+                continue
         
+            # Check for changes
             for file in filenames:
-                
                 newHashes[file] = self.__getFileHash(file)
                 
                 if(newHashes[file] != oldHashes[file]):
                     oldHashes[file] = newHashes[file]
                     callbacks["onChange"](file)
                                     
-            time.sleep(0.5) 
-        
-    def __onChange(self, string):
-        print "Change detected in [" + string + "]" 
+            time.sleep(0.1) 
+    
+    def __print(self, string):
+        t = Terminal()
+        timestamp = time.strftime("%H:%M:%S")
+        print t.bold + t.white + "[" + t.yellow + "Watchman v1.0" + t.white + "] " + t.normal + t.white + timestamp + " " + t.normal + string
+    
+    def __onChange(self, file):
+        self.__print("Change detected in [" + file + "]")
         
     def __onRemove(self, files):
-        print "Files deleted"
+        self.__print("Files " + str(files) + " deleted")
         
     def __onAdd(self, files):
-        print "Files added" 
+        self.__print("Files " + str(files) + " added")
  
-    def __getFiles(self, dir):
+    def __getFiles(self, dir, ignoreHidden):
         filenames = []
 
         for root, dirs, files in os.walk(dir):
-            files = [f for f in files if not f[0] == '.']
-            dirs[:] = [d for d in dirs if not d[0] == '.']
+            if(ignoreHidden == True):
+                files = [f for f in files if not f[0] == '.']
+                dirs[:] = [d for d in dirs if not d[0] == '.']
             
             for file in files:
                 filenames.append(os.path.join(root, file))
@@ -84,17 +98,14 @@ class FileWatcher:
         return str(format(hash, 'x')).zfill(8)
 
 
-def cb(string):
-    print "Change detected in [" + string + "]" 
 
-watcher = FileWatcher()
-watcher.watch()
+    
+def main():
+    watchman = Watchman()
+    watchman.watch()
 
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+	try:
+		main()
+	except KeyboardInterrupt:
+		sys.exit(1)
