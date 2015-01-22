2e49915bfe565b6a1301e9e22f3462aed6177b2f
Patrick Hennessy <ph7vc@mail.missouri.edu>
2015-01-21 20:29:50 -0600

    Watchman now picks up file deletion and addition

--- watchman.py
+++ watchman.py
@@ -5,39 +5,62 @@ from zlib import adler32
 
 class FileWatcher:
 
-    def watch(self, dir=".", callback=None):        
+    def watch(self, dir=".", callbacks=None):        
         oldHashes = {}
         newHashes = {}
         
-        if not callback:
-            callback = self.__defaultCallback
+        if not callbacks:
+            callbacks = {"onChange": self.__onChange, "onRemove": self.__onRemove, "onAdd": self.__onAdd}
         
         print "Listening for file system changes..."
         
         while True:
             filenames = self.__getFiles(dir)
-                        
-            for file in filenames:
-                if(oldHashes.get(file) == None):
+        
+            if(len(oldHashes.keys()) > len(filenames)):
+                filesRemoved = list(set(oldHashes.keys()) - set(filenames))
+
+                for file in filesRemoved:
+                    del(oldHashes[file])
+                    del(newHashes[file])
+                    
+                callbacks["onRemove"](filesRemoved)
+                 
+            elif(len(oldHashes.keys()) < len(filenames)):                
+                filesAdded = list(set(filenames) - set(oldHashes.keys()))
+
+                for file in filesAdded:
                     oldHashes[file] = self.__getFileHash(file)
                     newHashes[file] = oldHashes[file]
-                    continue
                     
+                callbacks["onAdd"](filesAdded)
+        
+            for file in filenames:
+                
                 newHashes[file] = self.__getFileHash(file)
                 
                 if(newHashes[file] != oldHashes[file]):
                     oldHashes[file] = newHashes[file]
-                    callback(file)
+                    callbacks["onChange"](file)
                                     
             time.sleep(0.5) 
         
-    def __defaultCallback(self, string):
+    def __onChange(self, string):
         print "Change detected in [" + string + "]" 
+        
+    def __onRemove(self, files):
+        print "Files deleted"
+        
+    def __onAdd(self, files):
+        print "Files added" 
  
     def __getFiles(self, dir):
         filenames = []
 
         for root, dirs, files in os.walk(dir):
+            files = [f for f in files if not f[0] == '.']
+            dirs[:] = [d for d in dirs if not d[0] == '.']
+            
             for file in files:
                 filenames.append(os.path.join(root, file))
 
